8370398250d7a78f5b88e30fea963fb1ebf15909
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2021-02-25 18:25:16 -0300

    Add challenges list and load it in the context

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,21 +1,21 @@
 import { useContext } from "react";
 import { ChallengesContext } from "../contexts/ChallengesContext";
+
 import styles from "../styles/componontes/ChallengeBox.module.css";
 
 export function ChallengeBox() {
-    const contextData = useContext(ChallengesContext);
-    const hasActiveChallenge = true;
+    const activeChallenge = useContext(ChallengesContext).activeChallenge;
 
     return (
         <div className={styles.challengeBoxContainer}>
-            { hasActiveChallenge ? (
+            { activeChallenge ? (
                 <div className={styles.challengeActive}>
-                    <header>Ganhe 400 xp</header>
+                    <header>Ganhe {activeChallenge.amount} xp</header>
 
                     <main>
-                        <img src="icons/body.svg" />
+                        <img src={`icons/${activeChallenge.type}.svg`} />
                         <strong>Novo desafio</strong>
-                        <p>Levante e corra!</p>
+                        <p>{activeChallenge.description}</p>
                     </main>
 
                     <footer>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,18 @@
 import { createContext, useState, ReactNode } from "react";
+import challenges from "../../challenges.json";
 
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
 interface ChallengesContextData {
     level: number;
     currentExperience: number;
     challengesCompleted: number;
     levelUp: () => void;
     startNewChallenge: () => void;
+    activeChallenge: Challenge;
 }
 interface ChallengesProviderProps {
     children: ReactNode;
@@ -17,17 +24,27 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [level, setLevel] = useState(1);
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
+    const [activeChallenge, setActiveChallenge] = useState(null);
 
     function levelUp() {
         setLevel(level + 1);
     }
 
     function startNewChallenge() {
-        console.log("creating challenge");
+        const randomChallengedIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengedIndex];
+        setActiveChallenge(challenge);
     }
 
     return (
-        <ChallengesContext.Provider value={{ level, challengesCompleted, currentExperience, levelUp, startNewChallenge }}>
+        <ChallengesContext.Provider value={{
+            level,
+            challengesCompleted,
+            currentExperience,
+            levelUp,
+            startNewChallenge,
+            activeChallenge,
+        }}>
             {children}
         </ChallengesContext.Provider>
     );
